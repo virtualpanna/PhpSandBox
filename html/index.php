7c9b2b9daa0809b1749a20f612e7b150667e690b
Davide <davide@d-devel.com>
2025-12-01 14:18:15 +0200

    Bootstrap suppport added via composer

--- html/index.php
+++ html/index.php
@@ -1,2 +1,20 @@
 
-<?php include "database.php";
+<?php require "vendor/autoload.php"; ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <!-- Bootstrap CSS -->
+    <link href="vendor/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Bootstrap JS and dependencies (Optional) -->
+    <script src="vendor/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
